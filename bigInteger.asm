546e9ca4e18063d8b293c14a4e7ce1ae06161f71
KeyboardGrater <dweltBadger@proton.me>
2025-10-27 21:34:14 -0400

    Completly Redid everything

--- bigInteger.asm
+++ bigInteger.asm
@@ -1,244 +1,244 @@
 .data
-
-# Input Buffer
-inputBuffer: .space 41
-
-# BigInteger structure
-.align 2
-BigInt1: .space 41                          # 40 9's = fits into 2^133 in binary, convert to bytes. 2^17, (rounded up from 16.625), enough room in remainder (.625 < 1 - 1/8), so signed included already
-BigInt2: .space 41
-BigIntSum: .space 41 
-
 # CONSTANTS
-BIGINT_SIZE = 41
+BIG_INT_MAX_SIZE = 40
+RETURN_VALUES_MAX_SIZE = 41
+
+# Data and variables
+BigInt1: .word 0                            # data type
+         .space BIG_INT_MAX_SIZE            # length of it
+
+BigInt2: .word 0
+         .space BIG_INT_MAX_SIZE                  
+
+Summation: .word 0
+           .space RETURN_VALUES_MAX_SIZE    # for overflow reasons
+
+Result: .word 0
+        .space RETURN_VALUES_MAX_SIZE
 
 # Strings
-inputPromptOne: .asciiz "Please give a number that has between and including 1-40 digits: "
-inputPromptTwo: .asciiz "Please give a second number that has between and including 1-40 digits: "
+inputPromptOne: .asciiz "Please enter the first number 1 - 40 digits:"
+inputPromptTwo: .asciiz "Please enter the second number 1 - 40 digits: "
+summationMessage: .asciiz "Sumation: "
+resultMessage: .asciiz "Result: "
 newLine: .asciiz "\n"
+
+buffer: .space 41                           # buffer for input
 
 .text
 .globl main
 main:
-
     # Get user input
-    li $v0, 4
+    
+    # Get the first number
+    li $v0, 4                           
     la $a0, inputPromptOne
     syscall
 
-    li $v0, 4
-    la $a0, newLine
-    syscall
-
-    # Input buffer
+    # Get first number (in the form of a string)
     li $v0, 8
-    la $a0, inputBuffer
-    la $a1, BIGINT_SIZE
+    la $a0, buffer
+    la $a1, BIG_INT_MAX_SIZE
     syscall
 
-
-    subu $sp, $sp, 8                        # for getting the size of both numbers                
-
-    # Load input buffer into BigInt1
-    la $a1, inputBuffer
+    # Call conversion function
+    la $a1, buffer
     la $a2, BigInt1
-    jal copyInput
+    jal string_conversion_to_int
 
-    # Get BigInt2
-    li $v0, 4
-    la $a0, newLine
-    syscall
-
+    # Get the second number
     li $v0, 4
     la $a0, inputPromptTwo
     syscall
 
-    li $v0, 4
-    la $a0, newLine
-    syscall
-
+    # Get second number (in the form of a string)
     li $v0, 8
-    la $a0, inputBuffer
-    la $a1, BIGINT_SIZE
+    la $a0, buffer
+    la $a1, BIG_INT_MAX_SIZE
     syscall
 
-    li $v0, 4
-    la $a0, newLine
-    syscall
-
-    # Load input buff into BigInt2
-    la $a1, inputBuffer
+    # Call conversion function for a second time
+    la $a1, buffer
     la $a2, BigInt2
-    jal copyInput
+    jal string_conversion_to_int
 
-    # ----- FOR TESTING ----- #
-    j printTesting
-    backToMainFromTesting:
-
-    # ----- FOR TESTING ----- #
-
-
-    # Adding
+    # Addition of the two BigInt structs
     la $a1, BigInt1
     la $a2, BigInt2
-    jal bigIntAddition
+    la $a3, Summation
+    jal bigint_add
 
+    # Print Additon
+    li $v0, 4
+    la $a0, summationMessage
+    syscall
+
+    la $a1, Summation
+    jal print_BigInt    
 
     j endProgram
 
-copyInput:
-    # t0 = buffer adress, t1 = bigInt (1 or 2) address, t2 = i, t3 = current_address, t4 = loaded character
-    # t5 = null character (0x00)
-    move $t0, $a1                           # move buffer address to a temporary register
-    move $t1, $a2                           # move BigInt (1 or 2)'s address to temporary register
-    li $t2, 0                               # i = 0
-    
-# ---------------------- ACCOUNT FOR FIRST DIGIT, IE if negative --------------------------------
-    copyLoop:
-        # Get charcter at i of buffer
-        add $t3, $t0, $t2                   # source_char = buffer_addr + i
-        lb $t4, 0($t3)                      # save source_char to temp reg                 
 
-        # Get the i of bigInt (1 or 2)
-        add $t3, $t1, $t2
+string_conversion_to_int:
+    # t0 = i,
+    # s0 = struct_addr
+
+    addi $sp, $sp, -8
+    sw $ra 0($sp)                                # save return address to stack
+    sw $s0, 4($sp)
+
+    move $t0, $a1                                # while it is the address of the string, since a string is an arr of char, it is also pointing at the first index of the char array. And since we are only going up by one, then add on in each iteration to register
+    move $s0, $a2
+
+    # Find the end of the buffer string
+    find_buffer_end:
+        # move $t3, $a1, $t0
+        lb $t3, 0($t0)
         
-        # Check if null or new line
-        li $t5, 0x00                        # null
-        beq $t5, $t4, copyLoopExit          
-        li $t5, 0x0A                        # new line
-        beq $t5, $t4, copyLoopExit
+        beq $t3, 0x0A, found_buffer_end      # exit loop if t3 == 'NULL' 
+        beq $t3, 0x00, found_buffer_end      # exit loop if t3 == '\n'
+        
+        addi $t0, $t0, 1
+        j find_buffer_end
 
-        # Else
-        addi $t4, $t4, -48
+    found_buffer_end:
+        addi $t0, $t0, -1                   # move one over, i.e. back to the actual last digit 
 
-        sb $t4, 0($t3)
+        # Convert the digits into rever order
+        addi $t1, $s0, 4
+        li $t2, 0
 
+    conversion_loop:
+        blt $t0, $a1, conversion_finished
+        lb $t3, 0($t0)
+
+        # Convert to the ascii equivalent
+        addi $t3, $t3, -48                  # from chart to digit
+        sb $t3, 0($t1)
+        addi $t1, $t1, 1
         addi $t2, $t2, 1
-        j copyLoop
 
-    copyLoopExit:
+        addi $t0, $t0, -1
+        j conversion_loop
 
-    # Save length of number (# of digits)
-    addi $t2, $t2, -1                       # the last digit
-    subu $sp, $sp, 4
-    sw $t2, 0($sp)
+    conversion_finished:
+        sw $t2, 0($s0)                      # Store the length within the struct
+
+        lw $s0, 4($sp)
+        lw $ra, 0($sp)
+        addi $sp, $sp, 8
+
     jr $ra
 
+# Addition
+bigint_add:
+    # a1 = BigInt1_addr, a2 = BigInt2_addr, a3 = Summation_addr
+    # t0, t1, t2, t3, t4, t5 = carry_flag , t6 =  , t7
 
+    # Save the return address
+    addi $sp, $sp, -4
+    sw $ra 0($sp)   
 
-# ----- TEST PRINTING ----- #
-printTesting:
-    # a1 = BigInt1_address, a2 = BigInt2_address, t0 = (0 = BigInt1, 1 = BigInt2, 2 = Done), t1 = i, t2 = holder
-    # t3 = stopper, t4
-    li $v0, 11
-    la $a0, '\n'
-    syscall
+    # Load the length of the structs
+    lw $t3, 0($a1)                          # BigInt1_size
+    lw $t4, 0($a2)                          # BigInt2_size
 
-    la $a1, BigInt1
+    # Create pointers
+    addi $t1, $a1, 4                        # num1
+    addi $t2, $a2, 4                        # num2
+    addi $t0, $a3, 4                        # summation
+
+    li $t5, 0                               # car
+    li $t6, 0                               # i
+
+    # Find the max length
+    move $t7, $t3
+    bge $t3, $t4, addition_loop
+    move $t7, $t4
+
+    addition_loop:
+        li $t8, 0
+        li $t9, 0
+
+        # Get digit from BigInt1 
+        bge $t6, $t3, skip_num_1         # when BigInt_2_size < BigInt_2_size
+        add $s0, $t1, $t6
+        lb $t8, 0($s0)
+
+        skip_num_1:
+        # Get a digit from BigInt2
+        bge $t6, $t4, skip_num_2
+        add $s0, $t2, $t6
+        lb $t9, 0($s0)
+
+        skip_num_2:
+        
+        # Add the digits and then carray
+        add $s0, $t8, $t9                   # s0 = t8 + t9
+        add $s0, $s0, $t5                   # s0 = s0 (t8 +t9) + t5
+        
+        # Calculate the new carry and digit
+        li $t5, 0
+        blt $s0, 10 , dont_carry            # if s0 < 10, then no carry needed
+        
+        # else (Carry)
+        addi $s0, $s0, -10                  # otherwise, subtract 10
+        li $t5, 1                           # load that carried one
+
+        dont_carry:
+        
+        # Store the result digit
+        add $s1, $t0, $t6
+        sb $s0, 0($s1)
+
+        addi $t6, $t6, 1
+        blt $t6, $t7, addition_loop
+
+        # Add the carry to the left spot
+        beq $t5, 0, addition_loop_exit
+        add $s1, $t0, $t6
+        sb $t5, 0($s1)
+        addi $t6, $t6, 1
     
-    li $t0, 0
-    li $t1, 0
-    li $t4, 2
+    addition_loop_exit:
+        # Store the length in struct
+        sw $t6, 0($a3)
 
-    printTestingLoop:
-        add $t2, $a1, $t1
-        lb $t2, 0($t2)
+        lw $ra, 0($sp)
+        addi $sp, $sp, 4
 
-        li $t3, 0x00
-        beq $t2, $t3, printTestingLoopExit
-        li $t3, 0x0A
-        beq $t2, $t3, printTestingLoopExit
+    jr $ra
 
+print_BigInt:
+    # a1 = BigInt_addr
+
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    # Load the length of the struct into a temporary register
+    lw $t1, 0($a1)
+
+    # pointers
+    addi $t0, $a1, 4
+    addi $t1, $t1, -1                       # start from the last index
+
+    bigInt_print_loop:
+        bltz $t1, bigInt_print_loop_exit
+        add $t2, $t0, $t1
+        lb $t3, 0($t2)
+        # addi $t3, $t3, 48                 # convert to ascii, I dont think I need it. The simulator (Qt spim) might be causing this to not be nessicary.
+
+        # Print the digit
         li $v0, 1
-        move $a0, $t2
+        move $a0, $t3
         syscall
-        
-        addi $t1, $t1, 1
-        j printTestingLoop
-    printTestingLoopExit:
 
-    addi $t0, $t0, 1
-    beq $t0, $t4, printTestingJump
-    la $a1, BigInt2
-    li $t1, 0
-
-    li $v0, 11
-    la $a0, '\n'
-    syscall
-
-    j printTestingLoop
-    
-
-    printTestingJump:
-    j backToMainFromTesting
-
-# ----- TEST PRINTING ----- #
-
-# Fill Space with zeros
-fillSpace:
-    li $t0, 0
-    li $t1, BIGINT_SIZE
-    #addi $t1, $t1, -1
-    la $a0, BigIntSum
-
-    fillLoop:
-        beq $t0, $t1, fillLoopExit
-        li $t2, 0
-        sb $t2, 0($a0)
-        addi $t0, $t0, 1
-        
-        j fillLoop
-
-    fillLoopExit:
-    j fillSpaceReturn1
-
-
-
-# ADDING
-bigIntAddition:
-    # t0 = i, t1 = BigInt1_addr, t2 = BigInt2_addr, t3 = curr_addr_one, t4 = curr_addr_two, t5 = value_at_one, t6 = value_at_two
-    # t7 = one_size/largest, t8 = two_size/ holder, t9 = carry
-    move $t1, $a1
-    move $t2, $a2
-
-
-    # 0 0 0 0 4 5 6 7 8 9 1
-    # 0 0 0 0 0 0 0 4 2 1 3
-
-    lw $t8, 0($sp)
-    addiu $sp, $sp, 4
-    lw $t7, 0($sp)
-    addiu $sp, $sp, 4
-    
-    bgt $t8, $t7, twoLargerThanOne          # changes t7 to be t8, else t7 stays the same
-    twoLargerThanOneReturn:
-    
-    move $t0, $t8                           # make i = largest_num_of_digits
-
-    additionLoop:
-        # Get the value of BigInt1[i]
-        add $t3, $t1, $t0
-        lb $t5, 0($t3)
-
-        # Get the value of BigInt[i]
-        add $t4, $t2, $t0
-        lb $t6, 0($t4)
-
-        # ----- TODO: ADD WHEN ONE IS LARGER THAN THE OTHER ----- #
-        # ----- HOPEFULLY IT FILLS IN ZERO FOR THE REST OF THE DATA ---- #
-        # ----- IF NOT I MIGHT HAVE TO ADD THOSE ZEROS ----- #
-
-        add $t8, $t5, $t6
-        
-        
-        addi $t0, $t0, -1
-        j additionLoop
-
-jr $ra
-
-twoLargerThanOne:
-    move $t8, $t9
-    j twoLargerThanOneReturn
+        addi $t1, $t1, -1
+        j bigInt_print_loop
+    bigInt_print_loop_exit:
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
 
 
 endProgram:
