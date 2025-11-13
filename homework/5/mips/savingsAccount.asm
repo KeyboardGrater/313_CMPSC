1fe4cd5cf8ce345c1cb1d12bca9f87c29118bb14
KeyboardGrater <DweltBadger@proton.me>
2025-11-13 16:39:37 -0500

    Laid the ground work for the SavingsAccount.asm

---
+++ homework/5/mips/savingsAccount.asm
@@ -0,0 +1,24 @@
+.data
+
+# Structures
+# typedef struct {
+#     unsigned int account_number;
+#     double annual_interest_rate;
+#     double savings_balance;
+# } SavingsAccount;
+
+
+# Strings
+
+.text
+.globl main
+main:
+    # ---------------------- Initialization Part -----------------
+    
+
+
+
+# End program
+end_program:
+    li $v0, 10
+    syscall
